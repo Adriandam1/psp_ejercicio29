e25d67cfb69ce64238cd8f0a465286e9b8c71099
Adriamdam1 <aabeijoncarbajo@danielcastelao.org>
2025-03-06 11:02:23 +0100

    mostrando proveedoresw y servicios messagedigest

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.security.Provider;
+import java.security.Security;
+import java.security.Provider.Service;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
+        // Listamos los proveedores de seguridad
+        Provider[] providers = Security.getProviders();
 
+        // Recorremos los proveedores
+        for (Provider provider : providers) {
+            // Obtenemos los servicios del proveedor
+            Set<Service> services = provider.getServices();
+            System.out.println("Proveedor: " + provider.getName()+"\n\tServicios que correspondan a algoritmos de hash (MessageDigest) :");
+
+            // Recorremos los servicios
+            for (Service service : services) {
+                // Si el servicio es de tipo MessageDigest
+                if ("MessageDigest".equals(service.getType())) {
+                    // Imprimimos el algoritmo
+                    System.out.println("\t\t"+service.getAlgorithm());
+                    //System.out.println("Proveedor: " + provider.getName()+"\nServicio: "+service.getAlgorithm());
+                }
+            }
+        }
 
     }
 }
